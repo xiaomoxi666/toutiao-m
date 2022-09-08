32736a91e08eda510caab07594c2ea8081611b10
baolei <fanbaolei2021@163.com>
2022-09-08 19:25:22 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-// 加载全局样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
 import './styles/index.less'
-
+Vue.use(Vant)
+// 加载全局样式
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
